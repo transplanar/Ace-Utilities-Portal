69a99241d5ff02d8b9198c46c2f0c411504e1ae9
Glen Cooney <glen@glencooney.com>
2018-05-09 01:29:53 +0000

    Initial commit

---
+++ app/javascript/components/app.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import PropTypes from "prop-types"
+// import SocialMediaMessageBuilder from "./SocialMediaMessageBuilder.jsx"
+// import HourlyCalculator from "./HourlyCalculator.jsx"
+// import SquareFootCalculator from "./SquareFootCalculator.jsx"
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  
+  handleChange(event){
+   
+  }
+  
+  render () {
+    return (
+      <div id="app">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
