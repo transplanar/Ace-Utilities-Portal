36b0c242df317f2c6b35ac5d325b185fbd3166e6
Glen Cooney <glen@glencooney.com>
2018-05-22 18:18:47 -0700

    Renamed a few things for clarity

--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -3,56 +3,56 @@ import PropTypes from "prop-types"
 import _ from 'lodash';
 
 import SocialMediaMessageBuilder from "./SocialMediaMessageBuilder"
-import ScreenCalculator from "./ScreenCalculator"
+import ScreenGlassCalculator from "./ScreenGlassCalculator"
 
 
 class App extends React.Component {
   constructor(props){
     super(props);
-    
+
     this.modes = {
       socialMedia: {
           label: "Social Media Message Builder",
           content: <SocialMediaMessageBuilder />
       },
-      screenCalculator: {
+      ScreenGlassCalculator: {
         label: 'Screen Cost Calculator',
-        content: <ScreenCalculator />
+        content: <ScreenGlassCalculator />
       }
-    }; 
-    
+    };
+
     this.state = {
         // mode: this.modes.socialMedia,
-        mode: this.modes.screenCalculator,
+        mode: this.modes.ScreenGlassCalculator,
         navButtons: ''
     };
   }
-  
+
   componentDidMount(){
     this.renderNavButtons();
   };
-  
+
   renderNavButtons(){
     var navButtons = _.map(this.modes, (mode) => {
       var label = mode.label;
       return <button key={label} onClick={()=>this.selectMode(mode)}>{label}</button>;
     });
-    
+
     this.setState({
       navButtons: navButtons
     });
   }
-  
+
   selectMode(mode){
       if(_.includes(this.modes, mode)){
         this.setState({
             mode: mode
-        });  
+        });
       }else{
         console.warn(`Invalid mode supplied: ${JSON.stringify(mode)}`);
       }
   };
-  
+
   render () {
     return (
       <div id="app">
